3ee27fa00bc4d198916e5457cb240ee22a74938e
GraceLunaVerdueta <62574500+GraceLunaVerdueta@users.noreply.github.com>
2025-08-30 13:02:28 -0400

    se actualizo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import { Routes, Route } from "react-router-dom";  // ⬅ add this
 
 function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
     </>
   );
 }
