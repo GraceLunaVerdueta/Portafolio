2bd9196c49e9c57dff0a7f4c107cb52b6d7d0591
GraceLunaVerdueta <62574500+GraceLunaVerdueta@users.noreply.github.com>
2025-08-30 13:38:44 -0400

    ultimo error

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "/Portafolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
